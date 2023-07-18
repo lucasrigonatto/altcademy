976fd9c07c2541c9b7eeb664c62ce7db0c84860c
lucasrigonato <lucas.rigonatto@gmail.com>
2023-07-18 13:12:41 -0300

    qualquer

---
+++ script.js
@@ -0,0 +1,11 @@
+$.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
